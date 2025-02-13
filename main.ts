32bcff8f19a416bf1455609b4da5b93aa7465f4c
Julian <81559529+pilzithegoat@users.noreply.github.com>
2025-02-13 08:43:59 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    if (Math.randomBoolean()) {
+        basic.showIcon(IconNames.Skull)
+    } else {
+        basic.showIcon(IconNames.Square)
+    }
+})
